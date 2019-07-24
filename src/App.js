624105d8c9df712af44540cab3030283fc6351e4
aisav <artur.isaverdyan@gmail.com>
2019-07-24 20:03:06 +0400

    useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Toggle from './Toggle';
 
 const App = () => {
+
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    document.title = name
+  })
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle/>
+      <form onSubmit={ e => {
+        e.preventDefault()
+        formSubmit(name, setName)
+      }}>
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+
+        <button>
+          Submit
+        </button>
+
+      </form>
     </div>
   );
 };
+
+const formSubmit = (val, setVal) => {
+  console.log(`email sent to ${val}`)
+  setVal('')
+}
 
 export default App;
