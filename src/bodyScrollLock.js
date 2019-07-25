9fc043144c2ea785e4f33a13ac8af1e953318b9c
aisav <artur.isaverdyan@gmail.com>
2019-07-25 22:04:02 +0400

    cleanUp useLayoutEffect

--- src/bodyScrollLock.js
+++ src/bodyScrollLock.js
@@ -3,6 +3,11 @@ import {useLayoutEffect} from 'react'
 function useBodyScrollLock() {
   useLayoutEffect(() => {
     document.body.style.overflow='hidden'
+
+    // return function cleanup () {
+    return   () => {
+      document.body.style.overflow=''
+    }
   })
 }
 
